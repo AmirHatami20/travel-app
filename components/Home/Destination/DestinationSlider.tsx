343a0d1a9e76d36a8b9b6ae1e6e00f0c9c9749e2
Amir Hatami <hatamiamir055@email.com>
2025-07-09 18:39:06 +0330

    Fix problem

--- components/Home/Destination/DestinationSlider.tsx
+++ components/Home/Destination/DestinationSlider.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import Image from "next/image";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Autoplay} from "swiper/modules";
-import {useCallback, useRef} from "react";
+import {useRef, useCallback} from "react";
 import {TfiAngleLeft, TfiAngleRight} from "react-icons/tfi";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import Image from "next/image";
 import {destinationData} from "@/data/data";
 
-const DestinationSlider = () => {
-
-    //@ts-expect-error
-    const sliderRef = useRef<Swiper | null>(null);
+const CustomSlider = () => {
+    const sliderRef = useRef(null);
 
     // Handle previous slide
     const handlePrev = useCallback(() => {
@@ -44,14 +42,12 @@ const DestinationSlider = () => {
                     500: {slidesPerView: 2},
                     1024: {slidesPerView: 3},
                     1200: {slidesPerView: 4},
-                    1400: {slidesPerView: 5}
+                    1400: {slidesPerView: 5},
                 }}
             >
                 {destinationData.map((item) => (
                     <SwiperSlide key={item.id}>
-                        {/* Image */}
                         <div className="relative h-[400px]">
-                            {/* Overlay */}
                             <div className="absolute inset-0 bg-black/25 rounded-lg"/>
                             <Image
                                 src={item.image}
@@ -70,22 +66,22 @@ const DestinationSlider = () => {
             {/* Custom navigation buttons */}
             <div>
                 <button
-                    onClick={handleNext}
-                    className="swiper-button right-12"
-                    aria-label="Next slide"
-                >
-                    <TfiAngleRight size={24}/>
-                </button>
-                <button
                     onClick={handlePrev}
                     className="swiper-button left-12"
                     aria-label="Previous slide"
                 >
                     <TfiAngleLeft size={24}/>
                 </button>
+                <button
+                    onClick={handleNext}
+                    className="swiper-button right-12"
+                    aria-label="Next slide"
+                >
+                    <TfiAngleRight size={24}/>
+                </button>
             </div>
         </div>
     );
 };
 
-export default DestinationSlider;
+export default CustomSlider;
